c7d5e5b9b3ef84bf45f377d22c3b42d990b1c687
quangvoduy484 <quangvoduy484@gmail.com>
2019-04-24 18:02:36 +0700

    header

--- WebSiteBanHang/App_Start/RouteConfig.cs
+++ WebSiteBanHang/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace WebSiteBanHang
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Homepage", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
