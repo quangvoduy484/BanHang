fa569e640b854696b49b2f010daecd56f0bae846
duc.nguyen <duc.nguyen@s3corp.com.vn>
2019-06-10 14:01:22 +0700

    edit formbody model login

---
+++ WebSiteBanHang/ViewModel/CustomerLoginMobile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebSiteBanHang.ViewModel
+{
+    public class CustomerLoginMobile
+    {
+        [Required(ErrorMessage = "Tên không để trống:")]
+        public string EmailorPhone { get; set; }
+
+        [Required(ErrorMessage = "Pass không để trống:")]
+        public string Password { get; set; }
+    }
+}
